72ac5242c3440a569b37abb8c60f22f2bd7a5108
Phuong Vu <phuongmous@gmail.com>
2023-10-17 16:35:23 +1100

    updated JS functions

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ const timeRemaining = document.getElementById('time-remaining');
 const scoreDisplay = document.getElementById('score');
 const startButton = document.querySelector('.start-button');
 const replayButton = document.querySelector('.replay-button');
-const cardBacks = document.querySelectorAll('.card-back');
 const cardFronts = document.querySelectorAll('.card-front');
 
 /*----- app's state (variables) -----*/
 const state = {
 time: 20,
 score: 0,
+firstCardSrc: null,
 firstCard: null,
-secondCard: null,
+secondCardSrc: null,
 gameStarted: false
 };
 
@@ -22,10 +22,23 @@ timeRemaining.innerText = state.time;
 
 /*----- event listeners -----*/
 createCardImages();
-//addEventListener when clicking on a card
-cards.forEach(card => card.addEventListener('click', function(event) {const currentCard = this; flipCard(event); checkForMatch(currentCard)}));
+
 //addEventListener when clicking on start button
+// the cards are clickable only after the game start button is clicked => game start button is hidden
+
+//addEventListener when clicking on a card
+cards.forEach(card => card.addEventListener('click', function cardClickHandler(event) {
+    const currentCard = this;
+    if ((!card.classList.contains('matched'))) {
+        card.classList.add('flipped');
+    } else {
+        return;
+    };
+    checkForMatch(currentCard)}
+    ));
+
 startButton.addEventListener('click', countdownTimer);
+
 //addEventListener when clicking on replay button
 //replayButton.addEventListener('click', resetBoard);
 
@@ -33,7 +46,10 @@ startButton.addEventListener('click', countdownTimer);
 // give random images to cardFronts
 
 function createCardImages() {
-    const images = ['email.png', 'gingerbread.png', 'jumper.png', 'reindeer.png', 'snowman.png', 'sock.png', 'sweets.png', 'tree.png', 'email.png', 'gingerbread.png', 'jumper.png', 'reindeer.png', 'snowman.png', 'sock.png', 'sweets.png', 'tree.png'];
+    const images = ['email.png', 'gingerbread.png', 'jumper.png', 'reindeer.png',
+                    'snowman.png', 'sock.png', 'sweets.png', 'tree.png', 'email.png',
+                    'gingerbread.png', 'jumper.png', 'reindeer.png', 'snowman.png',
+                    'sock.png', 'sweets.png', 'tree.png'];
     const imageArray = [...images];
     imageArray.sort(() => Math.random() - 0.5);
     console.log(imageArray[0])
@@ -44,32 +60,34 @@ function createCardImages() {
     });
 }
 
-function flipCard(event) {
-    const card = event.currentTarget;
-    card.classList.toggle('flipped');}
-
-// check if card 1 matches with card 2 or not
-// if yes -> score = +1 and disable 2 matched cards
-// if no -> score will remain and the cards will flip back
-
 function checkForMatch(currentCard) {
-    currentCardImage = currentCard.querySelector('img');
+    console.log('currentCard', currentCard);
     currentCardImageSource = currentCard.querySelector('img[src]').getAttribute('src');
-    if (state.firstCard == null) {
-        state.firstCard = currentCardImageSource;
+    if (state.firstCardSrc == null) {
+        state.firstCardSrc = currentCardImageSource;
+        state.firstCard = currentCard;
     } else {
-        state.secondCard = currentCardImageSource;
-        if (state.firstCard === state.secondCard) {
+        state.secondCardSrc = currentCardImageSource;
+        if (state.firstCardSrc === state.secondCardSrc) {
             state.score += 1;
             scoreDisplay.textContent = state.score;
+            state.firstCard.classList.add('matched');
+            currentCard.classList.add('matched');
+            state.firstCard = null;
+            state.firstCardSrc = null;
+            console.log('firstCardSrc', state.firstCardSrc);
             }
         else {
             setTimeout(() => {
-                currentCardImage.setAttribute('src', 'imgs/back-card.png');
-              }, 1000);
-        }
-        console.log(state.firstCard, state.secondCard);
-     }  
+                currentCard.classList.remove('flipped');
+                state.firstCard.classList.remove('flipped');
+                state.firstCardSrc = null;
+                state.secondCardSrc = null;
+                state.firstCard = null;
+              }, 1000);  
+            }  
+        
+     }
 }
 
 function countdownTimer() {
@@ -84,4 +102,8 @@ function countdownTimer() {
         }
     }, 1000);
 }
+
+// when the time = 0 => the cards are unable to clicked
 // click on replay button, the board resets and shuffle the cards
+// => the game start buttn is shown & replay button is hidden
+// write function resetBoard
